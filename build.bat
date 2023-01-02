f5a6107931629d33b2e432bf7d0dc6bc4455612f
Mukul Gandhi <gandhi.mukul@gmail.com>
2023-01-02 20:04:13 +0530

    xalanj commit : fixing build.bat file, for xalan-java repos to support builds with Java 1.8

--- build.bat
+++ build.bat
@@ -20,58 +20,41 @@ rem
 rem     build.bat: Build Xalan-J 2.x using Ant 
 rem     Usage: build [ant-options] [targets]
 rem     Setup:
-rem         - you should set JAVA_HOME
+rem         - you must set JAVA_HOME
 rem         - you can set ANT_HOME if you use your own Ant install
-rem         - JAVA_OPTS is added to the java command line
-rem         - PARSER_JAR may be set to use alternate parser (default:lib\endorsed\xercesImpl.jar)
+
 echo.
 echo Xalan-J 2.x Build
-echo -------------
+echo -----------------
 
-if not "%JAVA_HOME%" == "" goto setant
-:noJavaHome
-rem Default command used to call java.exe; hopefully it's on the path here
-if "%_JAVACMD%" == "" set _JAVACMD=java
-echo.
-echo Warning: JAVA_HOME environment variable is not set.
-echo   If build fails because sun.* classes could not be found
-echo   you will need to set the JAVA_HOME environment variable
-echo   to the installation directory of java.
-echo.
+if "%JAVA_HOME%"=="" goto noJavaHome
 
-:setant
-rem Default command used to call java.exe or equivalent
-if "%_JAVACMD%" == "" set _JAVACMD=%JAVA_HOME%\bin\java
+if exist "%JAVA_HOME%\lib\tools.jar" (
+   set _CLASSPATH=%JAVA_HOME%\lib\tools.jar
+)
+
+set _JAVACMD=%JAVA_HOME%\bin\java
 
 rem Default ANT_HOME to the one what user has set
 if not "%ANT_HOME%"=="" set _ANT_HOME=%ANT_HOME%
 if "%ANT_HOME%"=="" set _ANT_HOME=.
 
-if EXIST "%_ANT_HOME%\tools\ant.jar" (
+if exist "%_ANT_HOME%\tools\ant.jar" (
    set _ANT_JARS=%_ANT_HOME%\tools\ant.jar
 ) else (
    set _ANT_JARS=%_ANT_HOME%\lib\ant.jar;%_ANT_HOME%\lib\ant-launcher.jar
 )
 
-rem Default locations of jars we depend on to run Ant on our build.xml file
-rem Set our local vars to all start with _underscore
-set _PARSER_JAR=%PARSER_JAR%
-if "%_PARSER_JAR%" == "" set _PARSER_JAR=lib\endorsed\xercesImpl.jar
-set _XML-APIS_JAR=%XML-APIS_JAR%
-if "%_XML-APIS_JAR%" == "" set _XML-APIS_JAR=lib\endorsed\xml-apis.jar
-
-rem Attempt to automatically add system classes to _CLASSPATH
-rem Use _underscore prefix to not conflict with user's settings
-set _CLASSPATH=%CLASSPATH%
-if exist "%JAVA_HOME%\lib\tools.jar" set _CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
-if exist "%JAVA_HOME%\lib\classes.zip" set _CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\classes.zip
-set _CLASSPATH=%_ANT_JARS%;%_XML-APIS_JAR%;%_PARSER_JAR%;%_CLASSPATH%
+set _CLASSPATH=%_CLASSPATH%;%_ANT_JARS%
 
 @echo on
-"%_JAVACMD%" -mx1024m %JAVA_OPTS% -Dant.home="%ANT_HOME%" -classpath "%_CLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%_JAVACMD%" -mx1024m -Djava.endorsed.dirs=lib\endorsed -classpath %_CLASSPATH% org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 @echo off
 
 goto end
+
+:noJavaHome
+echo Warning: JAVA_HOME environment variable is not set
 
 :end
 rem Cleanup environment variables
@@ -79,6 +62,3 @@ set _JAVACMD=
 set _CLASSPATH=
 set _ANT_HOME=
 set _ANT_JARS=
-set _PARSER_JAR=
-set _XML-APIS_JAR=
-
