9d87cd2343a3ad7ebeb9414261c7619e03257d1a
Mukul Gandhi <gandhi.mukul@gmail.com>
2023-07-09 22:00:15 +0530

    committing improvements to implementation of, xpath 3.1 dynamic function calls. also committing a related working test case as well.

--- tests/org/apache/xalan/xpath3/DynamicFunctionCallTests.java
+++ tests/org/apache/xalan/xpath3/DynamicFunctionCallTests.java
@@ -116,5 +116,15 @@ public class DynamicFunctionCallTests extends XslTransformTestsUtil {
         
         runXslTransformAndAssertOutput(xmlFilePath, xslFilePath, goldFilePath, null);
     }
+    
+    @Test
+    public void xslDynamicFunctionCallTest8() {
+        String xmlFilePath = XSL_TRANSFORM_INPUT_DIRPATH + "test1_d.xml"; 
+        String xslFilePath = XSL_TRANSFORM_INPUT_DIRPATH + "test8.xsl";
+        
+        String goldFilePath = XSL_TRANSFORM_GOLD_DIRPATH + "test8.out";                
+        
+        runXslTransformAndAssertOutput(xmlFilePath, xslFilePath, goldFilePath, null);
+    }
 
 }
