2ac5a128cdb4ef36023a2f703f74c15b8b032758
kubycsolutions <keshlam@kubyc.solutions>
2023-11-06 15:13:10 -0500

    Yeah, it might be better to keep the xml-site stylesheets in a tar.gz and expand them at build time, but mvn doesn't have a simple untar (except via antrun or exec) and the're already checked in expanded, so...

--- stylebook_docgen.sh
+++ stylebook_docgen.sh
@@ -10,7 +10,7 @@
 
 stylebook_dir="./stylebook"
 stylebook_book="${stylebook_dir}/sources/xalan-jlocal.xml"
-stylebook_style="${stylebook_dir}/style"
+stylebook_style="${stylebook_dir}/stylebook-1.0-b3_xalan-2.expanded"
 doc_generator="org.apache.stylebook.StyleBook"
 doc_generator_styletar="${stylebook_dir}/xml-site-style.tar.gz"
 xalan_cmdline_class="org.apache.xalan.xslt.Process"
@@ -22,17 +22,17 @@ stylebook_class_path="stylebook/stylebook-1.0-b3_xalan-2.jar":tools/xalan2jdoc.j
 # Note --directory affects any following options, so put it at EOL here
 # Also note that this really should be unpacking into a scratch directory,
 # or not unpacking at all except during execution; Oh Well
-tar -xf ${doc_generator_styletar} --directory ${stylebook_dir}
+#tar -xf ${doc_generator_styletar} --directory ${stylebook_dir}
 
 echo "Generate Xalan-J 2.x design document"
-java -cp ${stylebook_class_path} ${doc_generator} loaderConfig=sbk:/style/loaderdesign.xml targetDirectory=./target/site/design/ ./stylebook/sources/xalandesign.xml ./stylebook/style
+java -cp ${stylebook_class_path} ${doc_generator} loaderConfig=sbk:/style/loaderdesign.xml targetDirectory=./target/site/design/ ./stylebook/sources/xalandesign.xml ./stylebook/stylebook-1.0-b3_xalan-2.expanded
 
 
 # Generate a PDF file, via collation, xml2fo.xsl, and FOP (if we have it)
 # In Fact, this whole sequence is not being called in our normal Ant-driven builds either.
 if false; then 
     java -cp ${stylebook_class_path} ${xalan_cmdline_class} -xsl ./stylebook/sources/xalan-collate.xsl -out ./stylebook/sources/xalan/xalan-collate.xml
-    java -cp ${stylebook_class_path} ${xalan_cmdline_class} -in ./stylebook/sources/xalan/xalan-collate.xml -xsl ./stylebook/style/stylesheets/xml2fo.xsl  -param resourceFile ../../sources/xalan/resources.xml -param project Xalan-Java -out ./target/site/sources/xalan/xalan-collate.fo
+    java -cp ${stylebook_class_path} ${xalan_cmdline_class} -in ./stylebook/sources/xalan/xalan-collate.xml -xsl ./stylebook/stylebook-1.0-b3_xalan-2.expanded/stylesheets/xml2fo.xsl  -param resourceFile ../../sources/xalan/resources.xml -param project Xalan-Java -out ./target/site/sources/xalan/xalan-collate.fo
 fi
 
 # NOTE: Without the loaderConfig, we get complaints from
@@ -41,7 +41,7 @@ fi
 # xslt shouild be called with design2project.xsl, so that seems
 # harmless.
 echo "Generate XSLTC Architectural documentation"
-java -cp ${stylebook_class_path} ${doc_generator} loaderConfig=sbk:/style/loaderdesign.xml targetDirectory=./target/site/xsltc/ ./stylebook/sources/xsltc.xml ./stylebook/style
+java -cp ${stylebook_class_path} ${doc_generator} loaderConfig=sbk:/style/loaderdesign.xml targetDirectory=./target/site/xsltc/ ./stylebook/sources/xsltc.xml ./stylebook/stylebook-1.0-b3_xalan-2.expanded
 
 # Diff tells me that the -jlocal output is almost identical to the
 # -jsite output despite the slight difference in the .xml files used
@@ -51,11 +51,11 @@ java -cp ${stylebook_class_path} ${doc_generator} loaderConfig=sbk:/style/loader
 # until I better grok why this duplication was done in the first place I'm
 # hesitant to remove it. -- jkesselm, 20231105
 echo "Generate xalan-jlocal documentation"
-java -cp ${stylebook_class_path} ${doc_generator} loaderConfig=sbk:/style/loaderdesign.xml targetDirectory=./target/site/xalan/local ./stylebook/sources/xalan-jlocal.xml ./stylebook/style
+java -cp ${stylebook_class_path} ${doc_generator} loaderConfig=sbk:/style/loaderdesign.xml targetDirectory=./target/site/xalan/local ./stylebook/sources/xalan-jlocal.xml ./stylebook/stylebook-1.0-b3_xalan-2.expanded
 
 echo "autodocs equivalent"
 mkdir -p ./target/site/xalan
-java -cp ${stylebook_class_path} ${doc_generator} loaderConfig=sbk:/style/loaderdesign.xml targetDirectory=./target/site/xalan ./stylebook/sources/xalan-jsite.xml ./stylebook/style
+java -cp ${stylebook_class_path} ${doc_generator} loaderConfig=sbk:/style/loaderdesign.xml targetDirectory=./target/site/xalan ./stylebook/sources/xalan-jsite.xml ./stylebook/stylebook-1.0-b3_xalan-2.expanded
 
 mkdir -p ./target/site/xsltc
 cp stylebook/sources/xsltc/README.x* target/site/xsltc/
