3e997d5857d5e35f78a74468c079f03965a101d6
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-11-29 11:08:35 +0700

    Refactor version classes for better testability
    For now still retaining static methods and code duplication between
    modules 'xalan' and 'serializer', at least make the code more easily
    testable.

--- xalan/src/main/java/org/apache/xalan/Version.java
+++ xalan/src/main/java/org/apache/xalan/Version.java
@@ -20,7 +20,6 @@
  */
 package org.apache.xalan;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.regex.Matcher;
@@ -35,14 +34,13 @@ import java.util.regex.Pattern;
  * org.apache.xalan.processor.Version class.</P>
  * <P>See also: org/apache/xalan/res/XSLTInfo.properties for
  * information about the version of the XSLT spec we support.</P>
+ *
  * @xsl.usage general
  */
-public class Version
-{
+public class Version {
   private static final String VERSION_NUMBER_PATTERN = "^(\\d+)[.](\\d+)[.](D)?(\\d+)(-SNAPSHOT)?$";
   private static final String NO_VERSION = "0.0.0";
 
-  private static String version = NO_VERSION;
   private static int majorVersionNum;
   private static int releaseVersionNum;
   private static int maintenanceVersionNum;
@@ -51,12 +49,24 @@ public class Version
   private static boolean snapshot;
 
   static {
-    readProperties();
-    parseVersionNumber();
+    parseVersionNumber(readVersionNumber());
   }
 
-  private static void readProperties() {
-    Properties pomProperties = new Properties();
+  static String readVersionNumber() {
+    try (InputStream fromResource = getPropertiesStream()) {
+      Properties properties = new Properties();
+      if (fromResource != null) {
+        properties.load(fromResource);
+        return properties.getProperty("version", NO_VERSION);
+      }
+    } catch (Exception e) {
+      new RuntimeException("Cannot read properties file to extract Xalan version number information: ", e)
+        .printStackTrace();
+    }
+    return NO_VERSION;
+  }
+
+  static InputStream getPropertiesStream() {
     // IMPLEMENTATION NOTE: Class.getResourceAsStream uses a *relative* path by
     // default, in contrast to Classloader.getResourceAsStream, which uses an
     // *absolute* one. This is not clearly documented in the JDK, only
@@ -67,37 +77,36 @@ public class Version
     // Because we expect the properties file to be in the same directory/package
     // as this class, the relative path comes in handy and as a bonus is also
     // relocation-friendly (think Maven Shade).
-    try (InputStream fromResource = Version.class.getResourceAsStream("version.properties")) {
-      if (fromResource != null) {
-        pomProperties.load(fromResource);
-          version = pomProperties.getProperty("version", NO_VERSION);
-        }
-      }
-    catch (IOException e) {
-      new RuntimeException("Cannot read properties file to extract Xalan version number information: ", e)
-        .printStackTrace();
-    }
+    return Version.class.getResourceAsStream("version.properties");
   }
 
-  private static void parseVersionNumber() {
+  static void parseVersionNumber(String version) {
+    resetVersionNumber();
+    if (version == null)
+      version = NO_VERSION;
     Matcher matcher = Pattern.compile(VERSION_NUMBER_PATTERN).matcher(version);
     if (matcher.find()) {
       majorVersionNum = Integer.parseInt(matcher.group(1));
       releaseVersionNum = Integer.parseInt(matcher.group(2));
       if (matcher.group(3) == null) {
         maintenanceVersionNum = Integer.parseInt(matcher.group(4));
-      }
-      else {
+      } else {
         developmentVersionNum = Integer.parseInt(matcher.group(4));
       }
       snapshot = matcher.group(5) != null && !matcher.group(5).isEmpty();
-    }
-    else {
+    } else {
       System.err.println(
         "Cannot match Xalan version \"" + version + "\" " +
-          "against expected pattern \"" + VERSION_NUMBER_PATTERN + "\""
+          "against expected pattern \"" + VERSION_NUMBER_PATTERN + "\", " +
+          "resetting version number to "+ NO_VERSION
       );
+      resetVersionNumber();
     }
+  }
+
+  private static void resetVersionNumber() {
+    majorVersionNum = releaseVersionNum = maintenanceVersionNum = developmentVersionNum = 0;
+    snapshot = false;
   }
 
   /**
@@ -107,13 +116,12 @@ public class Version
    *
    * @return String denoting our current version
    */
-  public static String getVersion()
-  {
-     return getProduct()+" "+getImplementationLanguage()+" "
-           +getMajorVersionNum()+"."+getReleaseVersionNum()+"."
-           +( (getDevelopmentVersionNum() > 0) ?
-               ("D"+getDevelopmentVersionNum()) : (""+getMaintenanceVersionNum()))
-           +(isSnapshot() ? "-SNAPSHOT" :"");
+  public static String getVersion() {
+    return getProduct() + " " + getImplementationLanguage() + " "
+      + getMajorVersionNum() + "." + getReleaseVersionNum() + "."
+      + ((getDevelopmentVersionNum() > 0) ?
+      ("D" + getDevelopmentVersionNum()) : ("" + getMaintenanceVersionNum()))
+      + (isSnapshot() ? "-SNAPSHOT" : "");
   }
 
   /**
@@ -121,24 +129,21 @@ public class Version
    *
    * @param argv command line arguments, unused.
    */
-  public static void main(String argv[])
-  {
+  public static void main(String argv[]) {
     System.out.println(getVersion());
   }
 
   /**
    * Name of product: Xalan.
    */
-  public static String getProduct()
-  {
+  public static String getProduct() {
     return "Xalan";
   }
 
   /**
    * Implementation Language: Java.
    */
-  public static String getImplementationLanguage()
-  {
+  public static String getImplementationLanguage() {
     return "Java";
   }
 
@@ -146,76 +151,71 @@ public class Version
   /**
    * Major version number.
    * Version number. This changes only when there is a
-   *          significant, externally apparent enhancement from
-   *          the previous release. 'n' represents the n'th
-   *          version.
-   *
-   *          Clients should carefully consider the implications
-   *          of new versions as external interfaces and behaviour
-   *          may have changed.
+   * significant, externally apparent enhancement from
+   * the previous release. 'n' represents the n'th
+   * version.
+   * <p>
+   * Clients should carefully consider the implications
+   * of new versions as external interfaces and behaviour
+   * may have changed.
    */
-  public static int getMajorVersionNum()
-  {
+  public static int getMajorVersionNum() {
     return majorVersionNum;
   }
 
   /**
    * Release Number.
    * Release number. This changes when:
-   *            -  a new set of functionality is to be added, eg,
-   *               implementation of a new W3C specification.
-   *            -  API or behaviour change.
-   *            -  its designated as a reference release.
+   * -  a new set of functionality is to be added, eg,
+   * implementation of a new W3C specification.
+   * -  API or behaviour change.
+   * -  its designated as a reference release.
    */
-  public static int getReleaseVersionNum()
-  {
+  public static int getReleaseVersionNum() {
     return releaseVersionNum;
   }
 
   /**
    * Maintenance Drop Number.
    * Optional identifier used to designate maintenance
-   *          drop applied to a specific release and contains
-   *          fixes for defects reported. It maintains compatibility
-   *          with the release and contains no API changes.
-   *          When missing, it designates the final and complete
-   *          development drop for a release.
+   * drop applied to a specific release and contains
+   * fixes for defects reported. It maintains compatibility
+   * with the release and contains no API changes.
+   * When missing, it designates the final and complete
+   * development drop for a release.
    */
-  public static int getMaintenanceVersionNum()
-  {
+  public static int getMaintenanceVersionNum() {
     return maintenanceVersionNum;
   }
 
   /**
    * Development Drop Number.
    * Optional identifier designates development drop of
-   *          a specific release. D01 is the first development drop
-   *          of a new release.
-   *
-   *          Development drops are works in progress towards a
-   *          compeleted, final release. A specific development drop
-   *          may not completely implement all aspects of a new
-   *          feature, which may take several development drops to
-   *          complete. At the point of the final drop for the
-   *          release, the D suffix will be omitted.
-   *
-   *          Each 'D' drops can contain functional enhancements as
-   *          well as defect fixes. 'D' drops may not be as stable as
-   *          the final releases.
+   * a specific release. D01 is the first development drop
+   * of a new release.
+   * <p>
+   * Development drops are works in progress towards a
+   * compeleted, final release. A specific development drop
+   * may not completely implement all aspects of a new
+   * feature, which may take several development drops to
+   * complete. At the point of the final drop for the
+   * release, the D suffix will be omitted.
+   * <p>
+   * Each 'D' drops can contain functional enhancements as
+   * well as defect fixes. 'D' drops may not be as stable as
+   * the final releases.
    */
-  public static int getDevelopmentVersionNum()
-  {
+  public static int getDevelopmentVersionNum() {
     return developmentVersionNum;
   }
 
   /**
    * Snapshot flag.
    * Specifies whether the version number has a "-SNAPSHOT" suffix,
-   *          which by Maven/Gradle conventions designates a
-   *          development version.
+   * which by Maven/Gradle conventions designates a
+   * development version.
    */
-  public static boolean isSnapshot()
-  {
+  public static boolean isSnapshot() {
     return snapshot;
   }
 }
