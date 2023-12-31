bd574aaa9405c0b54d59ed3831bc4cdfcc7f3d57
Mukul Gandhi <gandhi.mukul@gmail.com>
2023-12-31 22:45:34 +0530

    adding a working new test case for xsl:for-each-group instruction

--- tests/org/apache/xalan/xslt3/GroupingTests.java
+++ tests/org/apache/xalan/xslt3/GroupingTests.java
@@ -339,5 +339,15 @@ public class GroupingTests extends XslTransformTestsUtil {
         
         runXslTransformAndAssertOutput(xmlFilePath, xslFilePath, goldFilePath, null);
     }
+    
+    @Test
+    public void xslGroupingTest30() {
+        String xmlFilePath = XSL_TRANSFORM_INPUT_DIRPATH + "test30.xml"; 
+        String xslFilePath = XSL_TRANSFORM_INPUT_DIRPATH + "test30.xsl";
+        
+        String goldFilePath = XSL_TRANSFORM_GOLD_DIRPATH + "test30.out";
+        
+        runXslTransformAndAssertOutput(xmlFilePath, xslFilePath, goldFilePath, null);
+    }
 
 }
